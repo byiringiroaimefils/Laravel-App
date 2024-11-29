afda8857ffcb44c4b97dd5f623d2880b94fc52bf
byiringiroaimefils <aimefils173@example.com>
2024-11-29 21:47:18 +0200

    Tec: changes in  Home

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,10 +1,1 @@
-<x-layout>
-    <h2>All</h2>
-    <x-cards>
-        <h2>Hello World</h2>
-        <span>John Doe</span>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum, aliquam magni blanditiis quibusdam maxime <br>
-            provident ex eligendi aut temporibus quidem harum quis dolores? Optio eius a natus odio similique ipsum <br>
-            temporibus nemo odit minus </p>
-    </x-cards>
-</x-layout>
+
