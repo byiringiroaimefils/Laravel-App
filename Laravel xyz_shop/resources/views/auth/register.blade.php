493be2ec86705cd41564b1c4218588191d4b31b8
byiringiroaimefils <aimefils173@example.com>
2024-12-01 13:41:16 +0200

    Dev: developing laravel xyz_shop

---
+++ Laravel xyz_shop/resources/views/auth/register.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="">
+
+    <title>XYZ_SHOP || Sign Up</title>
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans text-gray-900 antialiased">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        <h2 class="text-white text-center font-extrabold">Log In to XYZ_SHOP</h2><br>
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 overflow-hidden sm:rounded-lg">
+            {{-- <x-guest-layout> --}}
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+
+                    <!-- Name -->
+                    <div>
+                        <x-input-label for="name" :value="__('Name')" />
+                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
+
+                    <!-- Email Address -->
+                    <div class="mt-4">
+                        <x-input-label for="email" :value="__('Email')" />
+                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                    <!-- Password -->
+                    <div class="mt-4">
+                        <x-input-label for="password" :value="__('Password')" />
+                        <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+
+                    <!-- Confirm Password -->
+                    <div class="mt-4">
+                        <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                        <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                    </div>
+                    <div class=" mt-4 mb-2">
+                        <button class="bg-white w-full p-2 rounded-md">
+                            Sign Up
+                        </button>
+                        <div class="mt-2">
+                            <span class="text-white">Create Account</span>
+    
+                            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                                href="{{ route('login') }}">
+                                Log in
+                            </a>
+                        </div>
+                    </div>
+                </form>
+            {{-- </x-guest-layout> --}}
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ Laravel xyz_shop/resources/views/dashboard.blade.php
@@ -0,0 +1,71 @@
+<x-app-layout>
+    <h2 class="font-semibold text-xl ml-5 md:translate-x-14 mx-auto mt-5 dark:text-gray-200 leading-tight">
+      Dashboard.
+    </h2>
+
+<div class="py-12">
+    <div class="flex flex-wrap gap-32 justify-center items-center px-5">
+        <div class="dark:bg-gray-800 w-full sm:w-80 h-60 shadow-sm sm:rounded-lg border-indigo-600 border-2">
+            <div class="p-6 text-gray-900 flex flex-col justify-center items-center dark:text-gray-100">
+                <h2 class="font-black text-2xl uppercase">Product</h2><br>
+                <p class="font-black text-5xl mt-5">1</p>
+            </div>
+        </div>
+        <div class="dark:bg-gray-800 w-full sm:w-80 h-60 shadow-sm sm:rounded-lg border-indigo-600 border-2">
+            <div class="p-6 text-gray-900 flex flex-col justify-center items-center dark:text-gray-100">
+                <h2 class="font-black text-2xl uppercase">In</h2><br>
+                <p class="font-black text-5xl mt-5">05</p>
+            </div>
+        </div>
+        <div class="dark:bg-gray-800 w-full sm:w-80 h-60 shadow-sm sm:rounded-lg border-indigo-600 border-2">
+            <div class="p-6 text-gray-900 flex flex-col justify-center items-center dark:text-gray-100">
+                <h2 class="font-black text-2xl uppercase">Out</h2><br>
+                <p class="font-black text-5xl mt-5">20</p>
+            </div>
+        </div>
+    </div> <br><br><br>
+    <div class="dark:bg-gray-800 dark:text-white shadow-sm sm:rounded-lg mx-4 sm:mx-16 p-10">
+       <div class="">  
+           <h2 class="font-black uppercase text-xl">Current Inventory</h2>
+       </div> <br><br>
+
+        <table class="w-full text-center">
+            <thead>
+                <tr class="text-lg">
+                    <th>ID</th>
+                    <th>NAME</th>
+                    <th>QUANTITY</th>
+                    <th>PRICE</th>
+                    <th>Total Price</th>
+                </tr>
+            </thead>
+            <tbody class="">
+                    <tr class="border-y-2 border-gray-600 text-lg hover:bg-slate-500/10">
+                        <td>01</td>
+                        <td>Product A</td>
+                        <td>20.00</td>
+                        <td>4000.00</td>
+                        <td>4000.00</td>
+                    </tr>
+                    <tr class="border-y-2 border-gray-600 text-lg hover:bg-slate-500/10">
+                        <td>02</td>
+                        <td>Product B</td>
+                        <td>20.00</td>
+                        <td>4000.00</td>
+                        <td>4000.00</td>
+                    </tr>
+                    <tr class="text-lg hover:bg-slate-500/10">
+                        <td>03</td>
+                        <td>Product C</td>
+                        <td>20.00</td>
+                        <td>4000.00</td>
+                        <td>4000.00</td>
+                    </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+<div class="text-center text-white">
+    <p> &#169;2024  xyz_Shop</p>
+</div>
+</x-app-layout>
